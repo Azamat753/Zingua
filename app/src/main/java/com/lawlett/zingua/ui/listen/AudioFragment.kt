eba038968492cdb736d6fd9fb3557b7acaee1af8
Azamat <azamat.nazar99@gmail.com>
2024-06-05 22:19:00 +0600

    +

--- app/src/main/java/com/lawlett/zingua/ui/listen/AudioFragment.kt
+++ app/src/main/java/com/lawlett/zingua/ui/listen/AudioFragment.kt
@@ -1,55 +1,83 @@
 package com.lawlett.zingua.ui.listen
 
+import android.Manifest
 import android.media.MediaPlayer
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import by.kirich1409.viewbindingdelegate.viewBinding
+import com.karumi.dexter.Dexter
+import com.karumi.dexter.PermissionToken
+import com.karumi.dexter.listener.PermissionDeniedResponse
+import com.karumi.dexter.listener.PermissionGrantedResponse
+import com.karumi.dexter.listener.PermissionRequest
+import com.karumi.dexter.listener.single.PermissionListener
 import com.lawlett.zingua.R
 import com.lawlett.zingua.databinding.FragmentAudioBinding
 
-@Suppress("LABEL_NAME_CLASH")
+
 class AudioFragment : Fragment(R.layout.fragment_audio) {
-    val binding: FragmentAudioBinding by viewBinding()
-    private lateinit var mediaPlayer: MediaPlayer
-    private lateinit var audioModel: AudioModel
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        audioModel = arguments?.getSerializable("audio") as AudioModel
-        initClickers()
+  val binding: FragmentAudioBinding by viewBinding()
+  private lateinit var mediaPlayer: MediaPlayer
+  private lateinit var audioModel: AudioModel
 
-        binding.imvPlayNoti.setOnClickListener {
-            if (!this::mediaPlayer.isInitialized) {
-                mediaPlayer = MediaPlayer.create(requireContext(), audioModel.listQuestionModels.first().audio!!)
-            }
-            binding.imvStopNotificat.setOnClickListener {
-                var a = R.raw.audio_two
-                if (mediaPlayer.isPlaying) {
-                    mediaPlayer.pause()
-                    mediaPlayer.seekTo(0)
-                }
-                return@setOnClickListener
-            }
-            mediaPlayer.start()
+  override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+    super.onViewCreated(view, savedInstanceState)
+    audioModel = arguments?.getSerializable("audio") as AudioModel
+    initClickers()
+    requestRecordAudioPermission()
+  }
+
+  private fun requestRecordAudioPermission() {
+    Dexter.withContext(requireContext())
+      .withPermission(Manifest.permission.RECORD_AUDIO)
+      .withListener(object : PermissionListener {
+        override fun onPermissionGranted(p0: PermissionGrantedResponse?) {}
+        override fun onPermissionDenied(p0: PermissionDeniedResponse?) {}
+        override fun onPermissionRationaleShouldBeShown(
+          p0: PermissionRequest?,
+          p1: PermissionToken?
+        ) {
         }
+      }).check()
+  }
 
+  override fun onDestroyView() {
+    super.onDestroyView()
+    binding.blast.release();
+  }
 
+  private fun initClickers() {
+    binding.btnAudioUnderstend.setOnClickListener {
+      val bundle = Bundle()
+      bundle.putSerializable("audio", audioModel)
+      findNavController().navigate(R.id.quizFragment, bundle)
     }
-
-    private fun initClickers() {
-        binding.btnAudioUnderstend.setOnClickListener {
-            val bundle = Bundle()
-            bundle.putSerializable("audio", audioModel)
-            findNavController().navigate(R.id.quizFragment, bundle)
+    binding.imvPlayNoti.setOnClickListener {
+      if (!this::mediaPlayer.isInitialized) {
+        mediaPlayer =
+          MediaPlayer.create(requireContext(), audioModel.listQuestionModels.first().audio!!)
+        val audioSessionId = mediaPlayer.audioSessionId
+        if (audioSessionId != -1) {
+          binding.blast.setAudioSessionId(audioSessionId)
         }
-    }
-
-    override fun onDestroy() {
-        if (this::mediaPlayer.isInitialized) {
-            mediaPlayer.stop()
-            mediaPlayer.release()
+      }
+      binding.imvStopNotificat.setOnClickListener {
+        if (mediaPlayer.isPlaying) {
+          mediaPlayer.pause()
+          mediaPlayer.seekTo(0)
         }
-        super.onDestroy()
+      }
+      mediaPlayer.start()
     }
+  }
+
+  override fun onDestroy() {
+    if (this::mediaPlayer.isInitialized) {
+      mediaPlayer.stop()
+      mediaPlayer.release()
+    }
+    super.onDestroy()
+  }
 }
